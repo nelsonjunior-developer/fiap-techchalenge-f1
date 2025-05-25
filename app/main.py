5f783061cd7688b1ce2e3ad4a0f43acbf06d46af
Nelson Ferreira Junior <juniorsp15@hotmail.com>
2025-05-25 10:30:22 +0200

    Fix error

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 # app/main.py
 
+import logging
+
 from fastapi import FastAPI
 from app.routes import producao, comercializacao, processamento, importacao, exportacao
 from app.routes import auth
+
+# Configura o logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="FIAP Tech Challenge API",
     description="API pública para consulta de dados da vitivinicultura (Embrapa).",
     version="1.0.0",
 )
+
+logger.info("### Inciando as rotas ###")
 
 # Registra as rotas de autenticação
 app.include_router(auth.router)
