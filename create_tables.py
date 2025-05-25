22560a4ad06911b5dd7f3e5322b6aad971955b18
Nelson Ferreira Junior <juniorsp15@hotmail.com>
2025-05-25 10:42:25 +0200

    Adds some logs

--- create_tables.py
+++ create_tables.py
@@ -9,11 +9,16 @@ import database.models.execution_status
 import database.models.error_log
 
 import logging
+import sys
 
-# Configura o logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-logger.info("### Criando tabelas no banco de dados remoto... ###")
-Base.metadata.create_all(bind=engine)
-logger.info("### Tabelas criadas com sucesso... ###")
+logger.info("### Iniciando criação de tabelas... ###")
+
+try:
+    Base.metadata.create_all(bind=engine)
+    logger.info("### Tabelas criadas com sucesso. ###")
+except Exception as e:
+    logger.error(f"Erro ao criar tabelas: {e}")
+    sys.exit(1)
